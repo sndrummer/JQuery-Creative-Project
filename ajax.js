8c0774a377f21e0b54fbf608c4147babcb046497
Samuel Nuttall <sndrummer@gmail.com>
2017-10-05 16:01:20 -0600

    cleaned up some code

--- ajax.js
+++ ajax.js
@@ -1,8 +1,5 @@
-var myJoke
 function getJoke()
 {
-    
-
     $("#Jokebox").empty();
     $("#Gif").empty();
     console.log("getting joke");
@@ -15,29 +12,17 @@ function getJoke()
         if (joke.includes("sex"||"gay")) {
             getJoke();
             return;
-        } else {    
-            
-        }
+        } 
         myJoke = joke;
         getGif();
         console.log("Here is my Joke " + myJoke);
         $("#Jokebox").append(
-            $("<p/>").text(joke),
-                             );
+            $("<p/>").text(joke),                 );
         }
         });
-        
-        
-       
 }
-
-
-
-
 function getGif()
 { 
-    //var joke = $("#Jokebox");
-   // console.log("Joke2: " + joke);
     joke = myJoke.replace(/ +/g, "-");
     var str = "Hello world!";
     //var searchString = myJoke.substring(1, (myJoke.length)/2);
@@ -48,19 +33,12 @@ function getGif()
             contentType: "application/json; charset=utf-8",
             dataType: "json",
             success: function(response) {
-                //var giphyURL = 
-                //Add random for array.length
 
            var imgURL = response.data[getRandomInt(0,response.data.length)].images.fixed_height.url;
             console.log("ImgURL: " + imgURL);
-                //debugger
-
                 $("#Gif").append(
                    "<img src=" + imgURL + "width=\"480\" height=\"262\"></img>"
                     );
-                    // "<iframe src=" + imgURL + "width=\"480\" height=\"262\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe>"
-               // <iframe src="https://giphy.com/embed/Lx8lyPHGfdNjq" width="480" height="262" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/guy-chuck-punch-Lx8lyPHGfdNjq">via GIPHY</a></p>
-                //$("#img_gif").attr("src", response.data[0].embed_url);
             }
         });
 }
